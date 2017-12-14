59ecb322458868788fc703a13082c500e0c22964
Moussa Sidibe <moussa.sidibe@yoti.com>
2017-12-14 12:03:36 +0000

    SDK-104 : Use version 2.0.1 of JS library for QR code button

--- yoti/site/YotiHelper.php
+++ yoti/site/YotiHelper.php
@@ -33,7 +33,7 @@ class YotiHelper
     /**
      * Yoti Button javascript library.
      */
-    const YOTI_BUTTON_JS_LIBRARY = 'https://sdk.yoti.com/clients/browser.2.0.0.js';
+    const YOTI_BUTTON_JS_LIBRARY = 'https://sdk.yoti.com/clients/browser.2.0.1.js';
 
     /**
      * Yoti files upload dir

a6395945fc31695fe15ca277027082588eddf1d1
Moussa Sidibe <moussa.sidibe@yoti.com>
2019-01-03 17:47:31 +0000

    SDK-730: Add the ability to pull a specific PHP SDK version

--- pack-plugin.sh
+++ pack-plugin.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-#!/bin/bash
+
+SDK_TAG=$1
+
 NAME="yoti-joomla-extension-edge.zip"
+DEFAULT_SDK_TAG="1.2.1"
 SDK_RELATIVE_PATH="sdk"
-curl https://github.com/getyoti/yoti-php-sdk/archive/master.zip -O -L
-unzip master.zip -d sdk
-mv sdk/yoti-php-sdk-master/src/* sdk
-rm -rf sdk/yoti-php-sdk-master
+
+if [ "$SDK_TAG" = "" ]; then
+    SDK_TAG=$DEFAULT_SDK_TAG
+fi
+
+echo "Pulling PHP SDK TAG $SDK_TAG.zip ..."
+
+curl https://github.com/getyoti/yoti-php-sdk/archive/$SDK_TAG.zip -O -L
+unzip $SDK_TAG.zip -d sdk
+mv sdk/yoti-php-sdk-$SDK_TAG/src/* sdk
+rm -rf sdk/yoti-php-sdk-$SDK_TAG
 
 
 if [ ! -d "./yoti" ]; then

32ee9a1f14f8d7029ff90b0d0708473eb8d40d84
David Grayston <davidgrayston@googlemail.com>
2019-07-01 23:54:56 +0100

    SDK-986: Split module test into reusable actions

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -19,4 +19,61 @@
 class AcceptanceTester extends \Codeception\Actor
 {
     use _generated\AcceptanceTesterActions;
+
+    /**
+     * Configures the Yoti Component.
+     */
+    public function configureTheYotiComponent() {
+        $I = $this;
+
+        // Browse to configuration form.
+        $I->click('Components', '#menu');
+        $I->click('Yoti', '#menu');
+
+        // Fill in form with test configuration.
+        $I->fillField('App ID', 'test_app_id');
+        $I->fillField('Scenario ID', 'test_scenario_id');
+        $I->fillField('SDK ID', 'test_sdk_id');
+        $I->fillField('Company Name', 'test_company_name');
+        $I->attachFile('input[type="file"][id="yoti_pem"]', 'test.pem');
+        $I->scrollTo('.btn-success');
+
+        $I->click('Save Settings');
+    }
+
+    /**
+     * Places the Yoti Module.
+     */
+    public function placeTheYotiModule() {
+        $I = $this;
+
+        // Browse to Yoti module.
+        $I->click('Extensions', '#menu');
+        $I->click('Modules', '#menu');
+        $I->scrollTo('#moduleList');
+        $I->click('Yoti Login', '#moduleList .pull-left');
+
+        // Set position.
+        $I->waitForElement('#jform_position_chzn');
+        $I->click('#jform_position_chzn');
+        $I->click('#jform_position_chzn [data-option-array-index="28"]');
+
+        // Publish.
+        $I->click('#jform_published_chzn');
+        $I->click('#jform_published_chzn [data-option-array-index="0"]');
+
+        // Clear publish end date.
+        $I->fillField('Finish Publishing', '');
+
+        $I->click('Save');
+
+        // Browser to menu assignment.
+        $I->click('Menu Assignment');
+
+        // Place on every page.
+        $I->click('#jform_assignment_chzn');
+        $I->click('#jform_assignment_chzn [data-option-array-index="0"]');
+
+        $I->click('Save');
+    }
 }
--- tests/acceptance/YotiModuleCest.php
+++ tests/acceptance/YotiModuleCest.php
@@ -5,39 +5,11 @@ class YotiModuleCest
     public function placeModule(AcceptanceTester $I)
     {
         $I->doAdministratorLogin();
-        $I->click('Components', '#menu');
-        $I->click('Yoti', '#menu');
-        $I->fillField('App ID', 'test_app_id');
-        $I->fillField('Scenario ID', 'test_scenario_id');
-        $I->fillField('SDK ID', 'test_sdk_id');
-        $I->fillField('Company Name', 'test_company_name');
-        $I->attachFile('input[type="file"][id="yoti_pem"]', 'test.pem');
-        $I->scrollTo('.btn-success');
-        $I->click('Save Settings');
+        $I->configureTheYotiComponent();
+        $I->placeTheYotiModule();
 
-        $I->click('Extensions', '#menu');
-        $I->click('Modules', '#menu');
-        $I->scrollTo('#moduleList');
-        $I->click('Yoti Login', '#moduleList .pull-left');
-
-        $I->see('Yoti Login');
-
-        $I->waitForElement('#jform_position_chzn');
-
-        $I->click('#jform_position_chzn');
-        $I->click('#jform_position_chzn [data-option-array-index="28"]');
-        $I->click('#jform_published_chzn');
-        $I->click('#jform_published_chzn [data-option-array-index="0"]');
-        $I->fillField('Finish Publishing', '');
-        $I->click('Save');
-
-        $I->click('Menu Assignment');
-        $I->click('#jform_assignment_chzn');
-        $I->click('#jform_assignment_chzn [data-option-array-index="0"]');
-        $I->click('Save');
-
-        // $I->amOnPage('/');
-        // $I->waitForElement('a[data-scenario-id="test_scenario_id"]');
-        // $I->see('a[data-scenario-id="test_scenario_id"][data-application-id="test_app_id"]');
+        $I->amOnPage('/');
+        $I->waitForElement('a[data-scenario-id="test_scenario_id"][data-application-id="test_app_id"]');
+        $I->see('Use Yoti');
     }
 }

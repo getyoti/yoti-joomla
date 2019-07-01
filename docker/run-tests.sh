fb793e92b4a338757264cfe1560dfe48e0985016
David Grayston <davidgrayston@googlemail.com>
2019-07-01 14:29:00 +0100

    SDK-986: Fixing joomlatools cache

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -2,11 +2,5 @@
 docker-compose build joomla-base
 docker-compose up -d joomla-dev
 
-# Install Joomla plugin.
-./install-joomla.sh joomla-dev
-
 # Coding Standards
 docker-compose exec joomla-dev sh -c "phpcs ./yoti-joomla"
-
-# Run tests.
-# docker-compose exec joomla-dev ./vendor/bin/phpunit
